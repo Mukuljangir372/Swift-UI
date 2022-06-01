f0faf82310530905f17976c57d051bf06ffbc8a4
mukul372 <mukul@scogo.in>
2022-06-01 16:16:53 +0530

    Initial Commit

---
+++ Swift UI/Swift_UIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Swift_UIApp.swift
+//  Swift UI
+//
+//  Created by Mukul Jangir on 01/06/22.
+//
+
+import SwiftUI
+
+@main
+struct Swift_UIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
